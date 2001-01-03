0949a79b27a3cc83dac6f011d274746609757e17
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2001-01-03 18:37:35 +0000

    version of 2001.01.03.18.37.35 (now is 01 Feb 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2411 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/tests/clusplot-out.R
+++ cluster/tests/clusplot-out.R
@@ -1,21 +1,27 @@
-### Clusplot() RESULT checking ...
+### SETUP
+library(cluster)
 
+### Clusplot() RESULT checking ...
 
 ## plotting votes.diss(dissimilarity) in a bivariate plot and
 ## partitioning into 2 clusters
 data(votes.repub)
 votes.diss <- daisy(votes.repub)
-votes.clus <- pam(votes.diss, 2, diss = TRUE)$clustering
-print.default(clusplot(votes.diss, votes.clus, diss = TRUE, shade = TRUE))
+for(k in 2:4) {
+    votes.clus <- pam(votes.diss, k, diss = TRUE)$clustering
+    print(clusplot(votes.diss, votes.clus, diss = TRUE, shade = TRUE))
+}
 
 ## plotting iris (dataframe) in a 2-dimensional plot and partitioning
 ## into 3 clusters.
 data(iris)
 iris.x <- iris[, 1:4]
-print.default(clusplot(iris.x, pam(iris.x, 3)$clustering, diss = FALSE))
+
+for(k in 2:5)
+    print(clusplot(iris.x, pam(iris.x, k)$clustering, diss = FALSE))
+
 
 .Random.seed <- c(0,rep(7654,3))
-
 ## generate 25 objects, divided into 2 clusters.
 x <- rbind(cbind(rnorm(10,0,0.5), rnorm(10,0,0.5)),
            cbind(rnorm(15,5,0.5), rnorm(15,5,0.5)))
