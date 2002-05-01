86583ed95a1247e77bb7eb6049982a6e13a4eaf4
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-05-01 14:31:45 +0000

    remove pre-1.2.0 compatibility code
    git-svn-id: https://svn.r-project.org/R-packages/trunk@51 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/src/foreign.h
+++ foreign/src/foreign.h
@@ -1,5 +1,5 @@
 /*
- *  $Id: foreign.h,v 1.1 2001/03/23 16:15:26 bates Exp $
+ *  $Id: foreign.h,v 1.2 2002/05/01 14:31:45 ripley Exp $
  *
  *  Common header file for the foreign package for R
  *
@@ -29,14 +29,6 @@
 
 #include "R.h"
 #include "Rinternals.h"
-
-/** R 1.2 compatibility definitions **/
-#if R_VERSION < R_Version(1, 2, 0)
-#define STRING_ELT(x,i)		(STRING(x)[i])
-#define VECTOR_ELT(x,i)         (VECTOR(x)[i])
-#define SET_STRING_ELT(x,i,v)	(STRING(x)[i] = (v))
-#define SET_VECTOR_ELT(x,i,v)	(VECTOR(x)[i] = (v))
-#endif /* R_VERSION */
 
 #define CN_TYPE_BIG     1
 #define CN_TYPE_LITTLE  2 
