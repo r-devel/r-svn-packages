e938d06f9af9d52d1178f1b5ef5dfa69cf2c4cd7
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-08-27 17:52:58 +0000

    Initial revision
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2565 694ef91d-65df-0310-b7bb-92e67a308ead

---
+++ cluster/src/cluster.h
@@ -0,0 +1,32 @@
+/* At least for the things in C */
+
+/* --------- clara.c ------------------*/
+int meet_(int *, int *);
+
+int
+black_(int *, int *, int *,
+       int *, int *, double *, double *, double *,
+       double *, double *, double *, int *, int *,
+       int *, int *, double *, double *);
+int
+selec_(int *, int *, int *,
+       int *, double *, int *, int *, int *,
+       double *, int *, int *, double *, double *,
+       int *, /*logical*/int *, double *, double *, double *,
+       int *, int *, int *, int *, int *, int *,
+       double *, double *);
+
+int
+randm_(int *, double *);
+int
+resul_(int *, int *, int *,
+       int *, int *, int *, double *, double *,
+       int *, int *);
+int
+bswap2_(int *, int *, int *,
+	double *, double *, double *, double *,
+	double *, double *);
+int
+dysta2_(int *, int *,
+	int *, double *, int *, double *, int *,
+	int *, double *, int *);
