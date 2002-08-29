bf502bcd999a1d5de4430a029518e13bfd27e8ad
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-08-29 17:51:21 +0000

    version of 2002.08.29.17.51.21 (now is 26 Jan 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2571 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/src/cluster.h
+++ cluster/src/cluster.h
@@ -1,32 +1,61 @@
 /* At least for the things in C */
 
-/* --------- clara.c ------------------*/
+/* in ./meet.f --- called from most source files: */
 int meet_(int *, int *);
 
-int
-black_(int *, int *, int *,
-       int *, int *, double *, double *, double *,
-       double *, double *, double *, int *, int *,
-       int *, int *, double *, double *);
-int
-selec_(int *, int *, int *,
-       int *, double *, int *, int *, int *,
-       double *, int *, int *, double *, double *,
-       int *, /*logical*/int *, double *, double *, double *,
-       int *, int *, int *, int *, int *, int *,
-       double *, double *);
+/* --------- ./clara.c ------------------*/
+#include <R_ext/Boolean.h>
 
-int
-randm_(int *, double *);
-int
-resul_(int *, int *, int *,
-       int *, int *, int *, double *, double *,
-       int *, int *);
-int
-bswap2_(int *, int *, int *,
-	double *, double *, double *, double *,
-	double *, double *);
-int
-dysta2_(int *, int *,
-	int *, double *, int *, double *, int *,
-	int *, double *, int *);
+void clara(int *n,  /* = number of objects */
+	   int *jpp,/* = number of variables */
+	   int *kk, /* = number of clusters, 1 <= kk <= n-1 */
+	   double *x,	/* Input:  the data x[n, jpp] _rowwise_ (transposed)
+			 * Output: the first `n' values are the `clustering'
+			 *	   (integers in 1,2,..,kk) */
+	   int *nran,	/* = #{random samples} drawn	   (= `samples' in R)*/
+	   int *nsam,	/* = #{objects} drawn from data set (`sampsize' in R) */
+	   double *dys,/* [1:(1 + (nsam * (nsam - 1))/2)]
+			* Output: to contain the distances */
+	   int *mdata,	/*= {0,1}; 1: min(x) is missing value (NA);  0: no NA */
+	   double *valmd,/*[j]= missing value code (instead of NA) for x[,j]*/
+	   int *jtmd,	/* [j]= {-1,1};	 -1: x[,j] has NA; 1: no NAs in x[,j] */
+	   int *ndyst,	/* = {1,2};  1 : euclidean;  2 : manhattan*/
+	   int *nrepr,	/* */
+	   int *nsel, int *nbest,
+	   int *nr, int *nrx,
+	   double *radus, double *ttd, double *ratt,
+	   double *ttbes, double *rdbes, double *rabes,
+	   int *mtt, double *obj, double *avsyl, double *ttsyl,
+	   double *sylinf, int *jstop,
+	   double *tmp, /* = double [ 3 * nsam ] */
+	   int *itmp	/* = integer[ 6 * nsam ] */
+    );
+
+void dysta2(int *nsam, int *jpp, int *nsel,
+	    double *x, int *n, double *dys, int *ndyst, int *jtmd,
+	    double *valmd, int *jhalt);
+
+void randm(int *nrun, double *ran);
+
+void bswap2(int kk, int nsam, int *nrepr,
+	    double *dys, double *sky, double s,
+	    double *dysma, double *dysmb, double *beter);
+
+void selec(int *kk, int *n, int *jpp, int *ndyst,
+	   double *zb, int *nsam, int *mdata, int *jtmd,
+	   double *valmd, int *nrepr, int *nsel, double *dys,
+	   double *x, int *nr, Rboolean *nafs,
+	   double *ttd, double *radus, double *ratt,
+	   int *nrnew, int *nsnew, int *npnew, int *ns, int *np, int *new,
+	   double *ttnew, double *rdnew);
+
+void resul(int *kk, int *n, int *jpp, int *ndyst,
+	   int *mdata, int *jtmd, double *valmd,
+	   double *x, int *nrx, int *mtt);
+
+void black(int kk, int jpp, int nsam, int *nbest,
+	   double *dys, double s, double *x,
+	   /* --> Output : */
+	   double *avsyl, double *ttsyl, double *sylinf,
+	   int *ncluv, int *nsend, int *nelem, int *negbr,
+	   double *syl, double *srank);
