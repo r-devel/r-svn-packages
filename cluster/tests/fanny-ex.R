48cb34a69cf4183374cb2d38af883fd029340ffe
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-09-02 17:11:04 +0000

    Initial revision
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2591 694ef91d-65df-0310-b7bb-92e67a308ead

---
+++ cluster/tests/fanny-ex.R
@@ -0,0 +1,29 @@
+
+library(cluster)
+
+###--- An extension of  example(fanny) : -------------------
+
+## generate 25 objects, divided into two clusters, and 3 objects lying
+## between those clusters.
+set.seed(171)
+x <- rbind(cbind(rnorm(10, 0, 0.5), rnorm(10, 0, 0.5)),
+           cbind(rnorm(15, 5, 0.5), rnorm(15, 5, 0.5)),
+           cbind(rnorm( 3,3.5,0.5), rnorm( 3,3.5,0.5)))
+
+data(ruspini)
+
+.proctime00 <- proc.time()
+
+(fannyx <- fanny(x, 2))
+summary(fannyx)
+str(fannyx)
+summary(fanny(x,3))# one extra
+
+summary(fanny(ruspini, 3), digits = 9)
+summary(fanny(ruspini, 4), digits = 9)# `correct' #{clusters}
+summary(fanny(ruspini, 5), digits = 9)
+
+## Last Line:
+cat('Time elapsed: ', proc.time() - .proctime00,'\n')
+## Lynne (P IV, 1.6 GHz):  7.5
+## nb-mm (P III,700 MHz): 12.4
