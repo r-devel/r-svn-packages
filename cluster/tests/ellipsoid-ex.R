8abbf23bc4c110a49565911258ea6b4515221c20
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-09-04 15:40:15 +0000

    Initial revision
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2605 694ef91d-65df-0310-b7bb-92e67a308ead

---
+++ cluster/tests/ellipsoid-ex.R
@@ -0,0 +1,18 @@
+library(cluster)
+
+eh <- ellipsoidhull(cbind(x=1:4, y = 1:4)) #singular
+eh
+
+set.seed(157)
+for(n in 2:10) {
+    cat("n = ",n,"\n")
+    x2 <- rnorm(n)
+    try(print(ellipsoidhull(cbind(1:n, x2))))
+    try(print(ellipsoidhull(cbind(1:n, x2, 4*x2 + rnorm(n)))))
+}
+
+x <- rt(100, df = 4)
+y <- 100 + 5 * x + rnorm(100)
+ellipsoidhull(cbind(x,y))
+z <- 10  - 8 * x + y + rnorm(100)
+ellipsoidhull(cbind(x,y,z))
