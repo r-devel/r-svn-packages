0465bd958ec5650e97041f0add02e566a01a9cd9
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-10-19 23:34:52 +0000

    version of 2002.10.19.23.34.52 (now is 26 Jan 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2661 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/src/mona.f
+++ cluster/src/mona.f
@@ -6,36 +6,33 @@ cc   Program for divisive hierarchical clustering of binary data,
 cc   using association analysis.
 cc
 cc   list of functions and subroutines:
-cc       function kab
+cc	 function kab
 
 c Args
       integer nn, pp, jerr
-cc	nn   = number of objects
-cc      pp   = number of variables
-cc      jerr : error return code in {1,2,3,4}
+c	nn   = number of objects
+c	pp   = number of variables
+c	jerr : error return code in {1,2,3,4}
       integer x(nn,pp), jlack(pp), nban(nn),ner(nn),kwan(nn),lava(nn)
+c	x[i,j]: binary (0/1) data (obs. i, var.j) -- missing values > 1 --
 
 c Function called:
       integer kab
 c VARs
+      logical syn
       integer j, ja, jb, jnat, jma, jtel, jtelz, jtel2, jres
       integer j0,j1, jptwe
-      integer jva, jvb, jvc, jvd
-      integer k, ka, kb, kal, kalf, kva, kvb, kvc, kvd, km
+      integer a, b, c, d, k, ka, kb, kal, kalf, km
       integer l, lbb, laa, lcc, ldd, lee, lack,lama, lams
-      integer nel, nelbb, nzf, nhalf, npass, nclu,nsyn, myst, mysca
+      integer nel, nelbb, nzf, nhalf, npass, nclu, myst, mysca
 
-      lack=0
       nhalf=(nn+1)/2
       jptwe=(pp+4)/5
       myst=0
       do 70 l=1,nn
-         mysca=0
-         do 60 j=1,pp
-            if(x(l,j).eq.0)go to 60
-            if(x(l,j).eq.1)go to 60
-c	    else: missing
-	    mysca=mysca+1
+	 mysca=0
+	 do 60 j=1,pp
+	    if(x(l,j) .gt. 1) mysca=mysca+1
  60	 continue
 	 if(mysca .eq. pp) then
 c     all variables missing for this object
@@ -45,16 +42,18 @@ c     all variables missing for this object
 	 myst=myst+mysca
  70   continue
       if(myst.eq.0)go to 290
+
+      lack=0
       do 100 j=1,pp
-         j0=0
-         j1=0
-         do 80 l=1,nn
-            if(x(l,j).eq.0)j0=j0+1
-            if(x(l,j).eq.1)j1=j1+1
- 80      continue
-         jlack(j)=nn-j0-j1
-         if(jlack(j).ne.0)lack=lack+1
-         if(jlack(j).ge.nhalf) then
+	 j0=0
+	 j1=0
+	 do 80 l=1,nn
+	    if(x(l,j).eq.0) j0=j0+1
+	    if(x(l,j).eq.1) j1=j1+1
+ 80	 continue
+	 jlack(j)=nn-j0-j1
+	 if(jlack(j).ne.0) lack=lack+1
+	 if(jlack(j).ge.nhalf) then
 c     at least 50% of the objects have missing values for this variable
 	    jerr=2
 	    return
@@ -68,58 +67,63 @@ c	    all non missing values are identical for this variable
 
       if(lack .eq. pp) then
 c     all variables have missing values
-         jerr=4
-         return
+	 jerr=4
+	 return
       endif
 cc
 cc   filling in missing values
 cc
       do 260 j=1,pp
-         if(jlack(j).eq.0)go to 260
-         lama=-1
-         nsyn=1
-         do 240 ja=1,pp
-            if(jlack(ja).ne.0)go to 240
-            jva=0
-            jvb=0
-            jvc=0
-            jvd=0
-            do 230 k=1,nn
-               if(x(k,j).eq.1)go to 220
-               if(x(k,ja).eq.0)jva=jva+1
-               if(x(k,ja).eq.1)jvb=jvb+1
-               go to 230
- 220           if(x(k,ja).eq.0)jvc=jvc+1
-               if(x(k,ja).eq.1)jvd=jvd+1
- 230        continue
-            kal=jva*jvd-jvb*jvc
-            kalf=kab(kal)
-            if(kalf.ge.lama)then
-               lama=kalf
-               jma=ja
-               if(kal.lt.0) nsyn=-1
-            endif
- 240     continue
-         do 250 l=1,nn
-            if(x(l,j).eq.0)go to 250
-            if(x(l,j).eq.1)go to 250
-            if(nsyn.eq.1)then
-               x(l,j)=x(l,jma)
-            else
-               if(x(l,jma).eq.1)x(l,j)=0
-               if(x(l,jma).eq.0)x(l,j)=1
-            endif
- 250     continue
+	 if(jlack(j) .ne. 0) then
+	    lama=-1
+	    syn=.true.
+	    do 240 ja=1,pp
+	       if(jlack(ja) .eq. 0) then
+c		no missing in x[, ja]
+		  a=0
+		  b=0
+		  c=0
+		  d=0
+		  do 230 k=1,nn
+		     if(x(k,j).eq.1)go to 220
+		     if(x(k,ja).eq.0)a=a+1
+		     if(x(k,ja).eq.1)b=b+1
+		     go to 230
+ 220		     if(x(k,ja).eq.0)c=c+1
+		     if(x(k,ja).eq.1)d=d+1
+ 230		  continue
+		  kal=a*d - b*c
+		  kalf=kab(kal)
+		  if(kalf.ge.lama)then
+		     lama=kalf
+		     jma=ja
+		     if(kal.lt.0) syn=.false.
+		  endif
+	       endif
+ 240	    continue
+
+	    do 250 l=1,nn
+	       if(x(l,j).gt. 1) then
+c		missing
+		  if(syn) then
+		     x(l,j)=x(l,jma)
+		  else
+		     if(x(l,jma).eq.1) x(l,j)=0
+		     if(x(l,jma).eq.0) x(l,j)=1
+		  endif
+	       endif
+ 250	    continue
+	 endif
  260  continue
-c--- end of treating missing values ----
+c---  end of treating missing values ----
 
 cc
 cc   initialization
 cc
  290  do 300 k=1,nn
-         kwan(k)=0
-         ner(k)=k
-         lava(k)=0
+	 kwan(k)=0
+	 ner(k)=k
+	 lava(k)=0
  300  continue
       npass=1
       kwan(1)=nn
@@ -133,40 +137,43 @@ C --- Loop ---
       lama=-1
       jnat=pp
       do 370 j=1,pp
-         if(nclu.eq.1)go to 330
-         j0=0
-         j1=0
-         do 325 l=ka,kb
-            nel=ner(l)
-            if(x(nel,j).eq.0)j0=j0+1
-            if(x(nel,j).eq.1)j1=j1+1
- 325     continue
-         if(j1.eq.0)go to 370
-         if(j0.eq.0)go to 370
- 330     jnat=jnat-1
-         lams=0
-         do 360 jb=1,pp
-            if(jb.eq.j)go to 360
-            kva=0
-            kvb=0
-            kvc=0
-            kvd=0
-            do 350 l=ka,kb
-               nel=ner(l)
-               if(x(nel,j).eq.1)go to 340
-               if(x(nel,jb).eq.0)kva=kva+1
-               if(x(nel,jb).eq.1)kvb=kvb+1
-               go to 350
- 340           if(x(nel,jb).eq.0)kvc=kvc+1
-               if(x(nel,jb).eq.1)kvd=kvd+1
- 350        continue
-            lams=lams+kab(kva*kvd-kvb*kvc)
- 360     continue
-         if(lams.le.lama)go to 370
-         jtel=kvc+kvd
-         jtelz=kva+kvb
-         lama=lams
-         jma=j
+	 if(nclu.eq.1)go to 330
+	 j0=0
+	 j1=0
+	 do 325 l=ka,kb
+	    nel=ner(l)
+	    if(x(nel,j).eq.0) j0=j0+1
+	    if(x(nel,j).eq.1) j1=j1+1
+ 325	 continue
+	 if(j1.eq.0)go to 370
+	 if(j0.eq.0)go to 370
+ 330	 jnat=jnat-1
+	 lams=0
+	 do 360 jb=1,pp
+	    if(jb .ne. j) then
+	       a=0
+	       b=0
+	       c=0
+	       d=0
+	       do 350 l=ka,kb
+		  nel=ner(l)
+		  if(x(nel,j).eq. 0) then
+		     if(x(nel,jb).eq.0) a=a+1
+		     if(x(nel,jb).eq.1) b=b+1
+		  else
+		     if(x(nel,jb).eq.0) c=c+1
+		     if(x(nel,jb).eq.1) d=d+1
+		  endif
+ 350	       continue
+	       lams=lams+kab(a*d - b*c)
+	    endif
+ 360	 continue
+	 if(lama .lt. lams) then
+	    lama=lams
+	    jtel =c+d
+	    jtelz=a+b
+	    jma=j
+	 endif
  370  continue
       if(jnat.lt.pp)go to 375
       kwan(ka)=-kwan(ka)
@@ -176,11 +183,11 @@ cc    splitting
 cc
  375  nel=ner(ka)
       if(x(nel,jma).eq.1)then
-         nzf=0
-         jtel2=jtel
+	 nzf=0
+	 jtel2=jtel
       else
-         nzf=1
-         jtel2=jtelz
+	 nzf=1
+	 jtel2=jtelz
       endif
       jres=kb-ka+1-jtel2
       km=ka+jtel2
@@ -193,15 +200,16 @@ c  -- inner loop --
       go to 390
 
  380  do 381 lbb=l,kb
-         nelbb=ner(lbb)
-         if(x(nelbb,jma).eq.nzf)go to 381
-         lcc=lbb-1
-         go to 382
+	 nelbb=ner(lbb)
+	 if(x(nelbb,jma) .ne. nzf) then
+	    lcc=lbb-1
+	    go to 382
+	 endif
  381  continue
  382  do 383 laa=l,lcc
-         ldd=lcc+l-laa
-         lee=ldd+1
-         ner(lee)=ner(ldd)
+	 ldd=lcc+l-laa
+	 lee=ldd+1
+	 ner(lee)=ner(ldd)
  383  continue
       ner(l)=nelbb
       go to 378
@@ -220,9 +228,10 @@ c  -- inner loop --
 
  500  npass=npass+1
       do 510 ka=1,nn
-         if(kwan(ka).ge.2)go to 310
+	 if(kwan(ka).ge.2) go to 310
  510  continue
       end
+
 cc
 cc kab(j) = |j|
 cc
