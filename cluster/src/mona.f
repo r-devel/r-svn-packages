4533e35f4d43594edc9513ab46d04e2dd4cebb10
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2002-10-19 22:37:36 +0000

    version of 2002.10.19.22.37.36 (now is 26 Jan 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2660 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/src/mona.f
+++ cluster/src/mona.f
@@ -1,4 +1,4 @@
-      subroutine mona(nn,jpp, kx,jerr, nban,ner,kwan,lava, jlack)
+      subroutine mona(nn,pp, x,jerr, nban,ner,kwan,lava, jlack)
 cc
 cc   MONothetic Analysis
 cc
@@ -9,68 +9,64 @@ cc   list of functions and subroutines:
 cc       function kab
 
 c Args
-      integer nn, jpp, jerr
+      integer nn, pp, jerr
 cc	nn   = number of objects
-cc      jpp  = number of variables
+cc      pp   = number of variables
 cc      jerr : error return code in {1,2,3,4}
-      integer kx(nn,jpp), jlack(jpp), nban(nn),ner(nn),kwan(nn),lava(nn)
+      integer x(nn,pp), jlack(pp), nban(nn),ner(nn),kwan(nn),lava(nn)
 
 c Function called:
       integer kab
 c VARs
-      integer j, jpres, ja, jb, jnat, jma, jtel, jtelz, jtel2, jres
-      integer jhalt,jnul,jeen, jptwe
+      integer j, ja, jb, jnat, jma, jtel, jtelz, jtel2, jres
+      integer j0,j1, jptwe
       integer jva, jvb, jvc, jvd
       integer k, ka, kb, kal, kalf, kva, kvb, kvc, kvd, km
       integer l, lbb, laa, lcc, ldd, lee, lack,lama, lams
-      integer nel, nelbb, nzf, nnhal, npass, nclu,nsyn, myst, mysca
+      integer nel, nelbb, nzf, nhalf, npass, nclu,nsyn, myst, mysca
 
       lack=0
-      jhalt=0
-      nnhal=(nn+1)/2
-      jptwe=(jpp+4)/5
+      nhalf=(nn+1)/2
+      jptwe=(pp+4)/5
       myst=0
       do 70 l=1,nn
          mysca=0
-         do 60 j=1,jpp
-            if(kx(l,j).eq.0)go to 60
-            if(kx(l,j).eq.1)go to 60
-            mysca=mysca+1
- 60      continue
-         myst=myst+mysca
-         if(mysca .eq. jpp) then
+         do 60 j=1,pp
+            if(x(l,j).eq.0)go to 60
+            if(x(l,j).eq.1)go to 60
+c	    else: missing
+	    mysca=mysca+1
+ 60	 continue
+	 if(mysca .eq. pp) then
 c     all variables missing for this object
-            jhalt=1
-            jerr=1
-         endif
+	    jerr=1
+	    return
+	 endif
+	 myst=myst+mysca
  70   continue
-      if(jhalt.eq.1)return
       if(myst.eq.0)go to 290
-      do 100 j=1,jpp
-         jnul=0
-         jeen=0
+      do 100 j=1,pp
+         j0=0
+         j1=0
          do 80 l=1,nn
-            if(kx(l,j).eq.0)jnul=jnul+1
-            if(kx(l,j).eq.1)jeen=jeen+1
+            if(x(l,j).eq.0)j0=j0+1
+            if(x(l,j).eq.1)j1=j1+1
  80      continue
-         jlack(j)=nn-jnul-jeen
+         jlack(j)=nn-j0-j1
          if(jlack(j).ne.0)lack=lack+1
-         if(jlack(j).ge.nnhal) then
+         if(jlack(j).ge.nhalf) then
 c     at least 50% of the objects have missing values for this variable
-            jhalt=1
-            jerr=2
-         endif
- 90      if(jnul.eq.0)go to 95
-         if(jeen.eq.0)go to 95
-         go to 100
-cc   all non missing values are identical for this variable
- 95      jhalt=1
-         jerr=3
+	    jerr=2
+	    return
+	 endif
+ 90	 if(j0.eq.0 .or. j1.eq.0) then
+c	    all non missing values are identical for this variable
+	    jerr=3
+	    return
+	 endif
  100  continue
-      if(jhalt.ne.0) return
 
-      jpres=jpp-lack
-      if(jpres.eq.0) then
+      if(lack .eq. pp) then
 c     all variables have missing values
          jerr=4
          return
@@ -78,23 +74,23 @@ c     all variables have missing values
 cc
 cc   filling in missing values
 cc
-      do 260 j=1,jpp
+      do 260 j=1,pp
          if(jlack(j).eq.0)go to 260
          lama=-1
          nsyn=1
-         do 240 ja=1,jpp
+         do 240 ja=1,pp
             if(jlack(ja).ne.0)go to 240
             jva=0
             jvb=0
             jvc=0
             jvd=0
             do 230 k=1,nn
-               if(kx(k,j).eq.1)go to 220
-               if(kx(k,ja).eq.0)jva=jva+1
-               if(kx(k,ja).eq.1)jvb=jvb+1
+               if(x(k,j).eq.1)go to 220
+               if(x(k,ja).eq.0)jva=jva+1
+               if(x(k,ja).eq.1)jvb=jvb+1
                go to 230
- 220           if(kx(k,ja).eq.0)jvc=jvc+1
-               if(kx(k,ja).eq.1)jvd=jvd+1
+ 220           if(x(k,ja).eq.0)jvc=jvc+1
+               if(x(k,ja).eq.1)jvd=jvd+1
  230        continue
             kal=jva*jvd-jvb*jvc
             kalf=kab(kal)
@@ -105,16 +101,18 @@ cc
             endif
  240     continue
          do 250 l=1,nn
-            if(kx(l,j).eq.0)go to 250
-            if(kx(l,j).eq.1)go to 250
+            if(x(l,j).eq.0)go to 250
+            if(x(l,j).eq.1)go to 250
             if(nsyn.eq.1)then
-               kx(l,j)=kx(l,jma)
+               x(l,j)=x(l,jma)
             else
-               if(kx(l,jma).eq.1)kx(l,j)=0
-               if(kx(l,jma).eq.0)kx(l,j)=1
+               if(x(l,jma).eq.1)x(l,j)=0
+               if(x(l,jma).eq.0)x(l,j)=1
             endif
  250     continue
  260  continue
+c--- end of treating missing values ----
+
 cc
 cc   initialization
 cc
@@ -133,21 +131,21 @@ cc
 C --- Loop ---
  310  kb=ka+kwan(ka)-1
       lama=-1
-      jnat=jpp
-      do 370 j=1,jpp
+      jnat=pp
+      do 370 j=1,pp
          if(nclu.eq.1)go to 330
-         jnul=0
-         jeen=0
+         j0=0
+         j1=0
          do 325 l=ka,kb
             nel=ner(l)
-            if(kx(nel,j).eq.0)jnul=jnul+1
-            if(kx(nel,j).eq.1)jeen=jeen+1
+            if(x(nel,j).eq.0)j0=j0+1
+            if(x(nel,j).eq.1)j1=j1+1
  325     continue
-         if(jeen.eq.0)go to 370
-         if(jnul.eq.0)go to 370
+         if(j1.eq.0)go to 370
+         if(j0.eq.0)go to 370
  330     jnat=jnat-1
          lams=0
-         do 360 jb=1,jpp
+         do 360 jb=1,pp
             if(jb.eq.j)go to 360
             kva=0
             kvb=0
@@ -155,12 +153,12 @@ C --- Loop ---
             kvd=0
             do 350 l=ka,kb
                nel=ner(l)
-               if(kx(nel,j).eq.1)go to 340
-               if(kx(nel,jb).eq.0)kva=kva+1
-               if(kx(nel,jb).eq.1)kvb=kvb+1
+               if(x(nel,j).eq.1)go to 340
+               if(x(nel,jb).eq.0)kva=kva+1
+               if(x(nel,jb).eq.1)kvb=kvb+1
                go to 350
- 340           if(kx(nel,jb).eq.0)kvc=kvc+1
-               if(kx(nel,jb).eq.1)kvd=kvd+1
+ 340           if(x(nel,jb).eq.0)kvc=kvc+1
+               if(x(nel,jb).eq.1)kvd=kvd+1
  350        continue
             lams=lams+kab(kva*kvd-kvb*kvc)
  360     continue
@@ -170,14 +168,14 @@ C --- Loop ---
          lama=lams
          jma=j
  370  continue
-      if(jnat.lt.jpp)go to 375
+      if(jnat.lt.pp)go to 375
       kwan(ka)=-kwan(ka)
       go to 400
 cc
 cc    splitting
 cc
  375  nel=ner(ka)
-      if(kx(nel,jma).eq.1)then
+      if(x(nel,jma).eq.1)then
          nzf=0
          jtel2=jtel
       else
@@ -189,14 +187,14 @@ cc
       l=ka
 c  -- inner loop --
  378  nel=ner(l)
-      if(kx(nel,jma).eq.nzf)go to 380
+      if(x(nel,jma).eq.nzf)go to 380
       l=l+1
       if(l.lt.km)go to 378
       go to 390
 
  380  do 381 lbb=l,kb
          nelbb=ner(lbb)
-         if(kx(nelbb,jma).eq.nzf)go to 381
+         if(x(nelbb,jma).eq.nzf)go to 381
          lcc=lbb-1
          go to 382
  381  continue
