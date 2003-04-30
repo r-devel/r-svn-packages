d88f03e5ac9d2aa801a49acb0e38714188f3bf17
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2003-04-30 14:01:11 +0000

    version of 2003.04.30.14.01.11 (now is 01 Feb 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2733 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/R/zzz.R
+++ cluster/R/zzz.R
@@ -3,23 +3,23 @@
   library.dynam("cluster", pkg, lib)
 }
 
-## only for ../tests/*.R :  week substitute for R versions < 1.4:
-if(!exists("identical", mode = "function"))
-    identical <- function(x,y) {## not the *proper* identical(), just a substitute!!
-        typeof(x) == typeof(y) &&
-        length(x) == length(y) &&
-        length(ax <- attributes(x)) == length(ay <- attributes(y)) &&
-        all(ax == ay) &&
-        all(x == y)
-    }
+## for R versions < 1.7:
+if(paste(R.version$major, R.version$minor, sep=".") < 1.7) {
 
-##  for R versions < 1.4:
-if(paste(R.version$major, R.version$minor, sep=".") < 1.4)
-    warning <- function (..., call. = TRUE) {
-        .Internal(warning(if (nargs() > 0) paste(..., sep = "")))
-    }
+    force <- function(x) x
 
-## basically for R versions < 1.3:
-if(!exists("dev.interactive", mode = "function"))
-    dev.interactive <- function ()
-    interactive() && .Device %in% c("X11", "GTK", "gnome", "windows", "Macintosh")
+    ## for R versions < 1.6:
+    if(paste(R.version$major, R.version$minor, sep=".") < 1.6) {
+        stop <- function (..., call. = TRUE)
+            .Internal(stop(if (nargs() > 0) paste(..., sep = "")))
+
+        ## for R versions < 1.5
+        if(paste(R.version$major, R.version$minor, sep=".") < 1.5)
+            ## cheap substitute, used in silhouette.default()
+            colSums <- function(x) apply(x, 2, sum)
+
+### NOTE: From cluster 1.7.0, we require at least R 1.4
+
+    }# versions < 1.6
+
+}# versions < 1.7
