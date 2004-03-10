e2eb0006c83521c24048c689b689e5a655e5e678
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2004-03-10 13:41:01 +0000

    version of 2004.03.10.13.41.01 (now is 01 Feb 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2798 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/tests/clara-NAs.R
+++ cluster/tests/clara-NAs.R
@@ -2,11 +2,19 @@ if(FALSE)
     library(cluster, lib.loc="/u/maechler/R/Pkgs/cluster.Rcheck")
 library(cluster)
 
-x <- cbind(c(0.05, -0.76, -4.4, -2.77, 0.12,  NA,  -5.48,  0.27, 2.01,
-             -0.24, 52.66, 50, 47.92, 50.78, 51.52, 51.69, 50.18),
-           c(-0.98, -2.67, NA, -6.97, -5.9, NA, 1.45, 0.43, -3.6,
-             2.66, 9.54, 12.85, 8.54,  NA,  NA, 8.93, 13.1))
+x <- cbind(c(0, -4, -22, -14, 0, NA, -28, 1, 10, -1,
+             100 + c(13, 0, 2, 4, 7, 8, 1)),
+           c(-5, -14, NA, -35, -30, NA, 7, 2, -18, 13,
+             47, 64, 48, NA, NA, 44, 65))
 x
-## call this repeatedly -> seg.fault (at "17" on lynne)
-for(N in 1:20){ cat(N,":\n"); cc <- clara(x, k=2, samples=2, sampsize=10)}
+(d <- dist(x,'manhattan'))
+summary(d, na.rm = TRUE) # max = 270
+## First call with "trace" (seg.fault typically later ...):
+try( clara(x, k=2, metric="manhattan", samples=2, sampsize=10, trace = 3) )
+## Originally:already shows the problem:  nbest[] = c(0,0,...,0) must be WRONG!!
+## Now: gives the proper error message.
 
+## S-plus 6.1.2 (rel.2 for Linux, 2002) gives
+##> cc <- clara(x, k=2, metric="manhattan", samples=2, sampsize=10)
+## Problem in .Fortran("clara",: Internal error: data for decrementing
+## ref. count didn't point to a valid arena (0x0), while calling subroutine clara
