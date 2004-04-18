9e392aed5159eb0ef8ad11abcb72a9c628fb1dc7
bates <bates@694ef91d-65df-0310-b7bb-92e67a308ead>
2004-04-18 16:21:00 +0000

    Moving packages to SVN control
    git-svn-id: https://svn.r-project.org/R-packages/trunk@251 694ef91d-65df-0310-b7bb-92e67a308ead

---
+++ nlme/R/zzMethods.R
@@ -0,0 +1,23 @@
+###   Miscellaneous methods that must be defined last in the library
+###
+### Copyright 1997-2003  Jose C. Pinheiro <Jose.Pinheiro@pharma.novartis.com>,
+###                      Douglas M. Bates <bates@stat.wisc.edu>
+
+## Note that  require( nls )  has already happened ...
+
+BIC.lme <- BIC.lmList <- BIC.gls <- BIC.lm
+
+comparePred.lme <- comparePred.lmList <- comparePred.gls
+
+getData.nlme <- getData.gnls
+
+getData.lme <- getData.gls <- getData.nls
+
+qqnorm.gls <- qqnorm.lm <- qqnorm.nls
+
+plot.lme <- plot.nls
+
+fitted.gnls <- fitted.gls
+
+residuals.gnls <- residuals.gls
+
