c984073abffabe59f58fe1c7b8029f829d509368
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2004-08-02 14:01:53 +0000

    use defaults for minpts, maxpts, eps; more logical maxpts default (for ndim >= 7) using rulcls --> adapt_1.0-3 (2002-03-14)
    git-svn-id: https://svn.r-project.org/R-packages/trunk@140 694ef91d-65df-0310-b7bb-92e67a308ead

--- adapt/R/adapt.R
+++ adapt/R/adapt.R
@@ -2,46 +2,51 @@
     library.dynam("adapt", pkg, lib)
 }
 
-adapt <- function (ndim, lower, upper, minpts, maxpts, functn, eps, ...)
+
+adapt <- function (ndim, lower, upper, minpts = 100, maxpts = NULL,
+                   functn, eps = 0.01, ...)
 {
     keep.trying <- is.null(maxpts)
-    if (is.null(maxpts)) maxpts <- max(minpts,500)
 
     if (ndim == 1) { ## fudge for 1-d functions
 	warning("Using integrate() from base package for 1-d integration")
+        if (keep.trying) maxpts <- minpts
 	return(integrate(functn,lower,upper,subdivisions=maxpts,rel.tol=eps,...))
     }
-    else ff <-
-	if(length(list(...)) && length(formals(functn)) > 1)
-	    function(x) functn(x, ...)
-	else functn # .Alias
+    ## else ndim >= 2 :
 
     ## Check to make sure that upper and lower are reasonable lengths
     ## Both the upper and lower limits should be at least of length ndim
-    ##
     if (length(lower) < ndim || length(upper) < ndim)#MM: dropped 'at least':
 	stop(paste("The lower and upper vectors need to have ndim elements\n",
 		   "Your parameters are:  ndim", ndim, ", length(lower)",
 		   length(lower), ", length(upper)", length(upper), "\n"))
+    ff <-
+	if(length(list(...)) && length(formals(functn)) > 1)
+	    function(x) functn(x, ...)
+	else functn # .Alias
+    rulcls <- 2^ndim + 2*ndim^2 + 6*ndim + 1 #-> ../src/adapt.f
 
-    if (minpts >= maxpts) {
-	warning(paste("maxpts must be > minpts.\n",
-		      "Maxpts has be increased to  minpts + 1"))
-	maxpts <- minpts + 1
+    ## maxpts should be large enough.  Prefer 10*rulclc, but use 2*rulclc.
+    if (keep.trying)
+        maxpts <- max(minpts, 500, 2 * rulcls)
+    else {
+        if (minpts >= maxpts) {
+            warning(paste("maxpts must be > minpts.\n",
+                          "Maxpts has be increased to  minpts + 1"))
+            maxpts <- minpts + 1
+        }
+        ##
+        if (maxpts < 2 * rulcls) {
+            warning(paste("You have maxpts (= ", maxpts, ") too small\n",
+                          "It needs to be at least 2 times 2^ndim + 2*ndim^2 + 6*ndim+1\n",
+                          "It has been reset to ", 2 * rulcls, "\n", sep=""))
+            maxpts <- 2 * rulcls
+        }
     }
-    ## rulcls and lenwrk are mandated in the adapt source
-    ##
-    repeat{
-	rulcls <- 2^ndim + 2*ndim^2 + 6*ndim + 1
-	lenwrk <- (2*ndim + 3) * (1 + maxpts/rulcls)/2
-	## maxpts should be large enough.  Prefer 10*rulclc, but use 2*rulclc.
-	##
-	if (maxpts < 2 * rulcls) {
-	    warning(paste("You have maxpts (= ", maxpts, ") too small\n",
-			  "It needs to be at least 2 times 2^ndim + 2*ndim^2 + 6*ndim+1\n",
-			  "It has been reset to ", 2 * rulcls, "\n", sep=""))
-	    maxpts <- 2 * rulcls
-	}
+
+    repeat {
+	lenwrk <- (2*ndim + 3)* (1 + maxpts/rulcls)/2# mandated in adapt source
 
 	x <- .C("cadapt",
 		as.integer(ndim),
@@ -55,7 +60,8 @@ adapt <- function (ndim, lower, upper, minpts, maxpts, functn, eps, ...)
 		relerr = double(1),
 		lenwrk = as.integer(lenwrk),
 		value = double(1),	# will contain the value of the integral
-		ifail = integer(1))[
+		ifail = integer(1),
+                PACKAGE = "adapt")[
                 c("value","relerr","minpts", "lenwrk", "ifail")]
 
 	if (x$ifail == 1 && keep.trying)
