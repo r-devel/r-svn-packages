6c7f97a56c8666e1269cbe12804f499c8dd6cf70
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2004-12-02 10:25:00 +0000

    add "verbose=TRUE" example to regr.tests
    git-svn-id: https://svn.r-project.org/R-packages/trunk@792 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/tests/S3.R
+++ foreign/tests/S3.R
@@ -2,7 +2,7 @@ library(foreign)
 read.S("mySobj")
 data.restore("dumpdata", print = TRUE)
 print(myobj)
-data.restore("tsdumpdata", print = TRUE)
+data.restore("tsdumpdata", print = TRUE, verbose = TRUE)
 sunspot
 utils::str(carbon.dioxide)
 carbon.dioxide[1:60]
