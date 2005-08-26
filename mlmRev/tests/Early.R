56ed4e46cd63bfbf741564a7c13329fa80641fc6
bates <bates@694ef91d-65df-0310-b7bb-92e67a308ead>
2005-08-26 22:03:53 +0000

    Release 0.98-1
    git-svn-id: https://svn.r-project.org/R-packages/trunk@1762 694ef91d-65df-0310-b7bb-92e67a308ead

--- mlmRev/tests/Early.R
+++ mlmRev/tests/Early.R
@@ -1,5 +1,5 @@
 library(mlmRev)
 options(show.signif.stars = FALSE)
 Early$tos <- Early$age - 0.5
-(fm1 <- lmer(cog ~ tos * trt + (tos|id), Early, con = list(EMv = 1, msV = 1)))
+(fm1 <- lmer(cog ~ tos * trt + (tos|id), Early))
 q("no")
--- mlmRev/tests/ScotsSec.R
+++ mlmRev/tests/ScotsSec.R
@@ -1,7 +1,11 @@
 library(mlmRev)
 options(show.signif.stars = FALSE)
+cntr <- list()
+#cntr <- list(EMverbose = TRUE, msVerbose = 1) # uncomment for verbose output
 (fm1 <- lmer(attain ~ verbal * sex + (1|primary) + (1|second), ScotsSec,
-             control = list(EMv = 1, msV = 1)))
+             control = cntr))
 (fm2 <- lmer(attain ~ verbal * sex + (1|primary) + (1|second), ScotsSec,
-             control = list(EMv = 1, msV = 1, niterEM = 40)))
+             control = c(cntr, list(niterEM = 40))))
+## fm1 and fm2 should be essentially identical when optimizing with nlminb
+## The fits are substantially different when optimizing with optim
 q("no")
