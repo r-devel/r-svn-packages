b0048a950f6f54a3fd142b4448ada0e01db07f8a
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2006-01-02 13:19:34 +0000

    update FSF address remove CVS $Id fields
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2109 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/R/minitab.R
+++ foreign/R/minitab.R
@@ -1,4 +1,3 @@
-### $Id: minitab.R,v 1.2 2001/06/09 14:40:20 hornik Exp $
 ###
 ###             Read stored Minitab worksheets
 ###
@@ -18,8 +17,8 @@
 ###
 ### You should have received a copy of the GNU General Public
 ### License along with this program; if not, write to the Free
-### Software Foundation, Inc., 59 Temple Place - Suite 330, Boston,
-### MA 02111-1307, USA
+### Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+### Boston, MA 02110-1301  USA
 
 ## Files in the Minitab portable worksheet format represent numbers in a
 ## fixed format (written in Fortran as 5e15.9 or something like that) but
