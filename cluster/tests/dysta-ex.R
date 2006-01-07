14675938048655967d908a28022830233a8a2d30
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2006-01-07 16:59:28 +0000

    version of 2006.01.07.16.59.28 (now is 01 Feb 2006).
    git-svn-id: https://svn.r-project.org/R-packages/trunk@2956 694ef91d-65df-0310-b7bb-92e67a308ead

--- cluster/tests/dysta-ex.R
+++ cluster/tests/dysta-ex.R
@@ -1,32 +1,58 @@
-#### Interface to intermediate level Fortran Routine
-####
-## Purpose: explore different versions of  dysta(), dysta2(), .. in
-##          Fortran code.
-dysta <- function(x, kind = c("euclidean","manhattan"))
-{
-  ## Purpose:
-  ## -------------------------------------------------------------------------
-  ## Arguments:
-  ## -------------------------------------------------------------------------
-  ## Author: Martin Maechler, Date:  3 Sep 2002, 08:21
+#### Interface to intermediate level Fortran Routines
+#### Purpose : explore different versions of  dysta(), dysta3(), .. in
+####	       Fortran code.
 
-    kind <- arg.match(kind)
+library(cluster)
+
+dysta <- function(x, kind = c("euclidean","manhattan"), dystaK = "dysta")
+{
+    ## Purpose:
+    ## -------------------------------------------------------------------------
+    ## Arguments:
+    ## -------------------------------------------------------------------------
+    ## Author: Martin Maechler, Date:  3 Sep 2002, 08:21
+
+    kind <- match.arg(kind)
     n <- nrow(x <- as.matrix(x))
     p <- ncol(x)
     storage.mode(x) <- "double"
-    hasNA <- apply(x, function(cl) any(is.na(cl)), 2)
+    hasNA <- apply(x, 2, function(cl) any(is.na(cl)))
     if(any(hasNA)) {
+	ina <- is.na(x)
+	x[ina] <- valmd <- -1.1*max(abs(range(x, na.rm = TRUE)))
+	valmd <- rep(valmd, p)
+    } else valmd <- 0.
 
+    r <- .Fortran(dystaK,
+	     n,
+	     jp = p,
+	     x,
+	     dys=   double(1 + n*(n-1)/2),
+	     ndyst= as.integer(kind == "euclidean"),# 1 or 0
+	     jtmd=  as.integer(ifelse(hasNA, -1, 1)),
+	     valmd= valmd,
+	     jhalt= integer(1),
+	     PACKAGE = "cluster")[c("dys", "jhalt")]
+    if(r$jhalt) {
+	cat("`jhalt' was ", r$jhalt,
+	    " -- some dissimilarities will be missing.\n")
+	r$dys[r$dys == -1.] <- NA
     }
-    .Fortran("dysta",
-             n,
-             jp = p,
-             x,
-             dys= double(1 + n*(n-1)/2),
-             ndyst=as.integer(kind == "euclidean"),
-             jtmd=as.integer(hasNA)
-             valmd=double()
-             jhalt=integer(1)
-             )
+    r$dys
+}
 
-         }
+(x <- cbind(c(0:6,NA), c(1,2,NA,7,NA,8:9,8)))
+dysta(x)
+(d1 <- dysta(x, kind = "m"))
+(d3 <- dysta(x, kind = "m",dystaK = "dysta3"))
+
+identical(sort(d1), sort(d3)) # TRUE
+cbind(d1=d1[-1], d3=d3[-length(d3)],
+      dist=dist(x,"manhattan"), daisy= daisy(x,"manhattan"))
+
+identical(d3[-length(d3)],
+	  c(dist(x,"manhattan")))# !
+identical(c(daisy(x,"manhattan")), c(dist(x,"manhattan")))
+identical(c(daisy(x,"euclidean")), c(dist(x,"euclidean")))
+identical(dysta(x, dystaK="dysta3")[-length(d3)],
+	  c(dist(x,"euclidean")))# !
