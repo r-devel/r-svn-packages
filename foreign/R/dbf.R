171545bd19b11c592c7c0b69270f960176dd5002
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2006-05-26 17:11:05 +0000

    allow for longer character fields in .dbf
    git-svn-id: https://svn.r-project.org/R-packages/trunk@3179 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/R/dbf.R
+++ foreign/R/dbf.R
@@ -25,7 +25,7 @@ read.dbf <- function(file, as.is = FALSE)
 }
 
 
-write.dbf <- function(dataframe, file, factor2char = TRUE)
+write.dbf <- function(dataframe, file, factor2char = TRUE, max_nchar = 254)
 {
 ### need to check precision
     allowed_classes <- c("logical", "integer", "numeric", "character",
@@ -72,11 +72,14 @@ write.dbf <- function(dataframe, file, factor2char = TRUE)
             rx <- range(x, na.rm = TRUE)
             rx[!is.finite(rx)] <- 0 # added RSB 2005-04-17
             mrx <- max(ceiling(log10(abs(rx))))
-            scale[i] <- min(precision[i] - ifelse(mrx > 0, mrx+3, 3), 15) 
+            scale[i] <- min(precision[i] - ifelse(mrx > 0, mrx+3, 3), 15)
                     # modified RSB 2005-03-10 and 2005-04-17
         } else if (is.character(x)) {
             mf <- max(nchar(x[!is.na(x)]))
-            precision[i] <- min(max(nlen, mf), 254)
+            p <- max(nlen, mf)
+            if(p > max_nchar)
+                warning(gettext("character column %d will be truncated to %d bytes", i, max_nchar), domain = NA)
+            precision[i] <- min(p, max_nchar)
             scale[i] <- 0
         } else stop("unknown column type in data frame")
     }
