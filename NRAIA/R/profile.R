f469cf7432563a6022f1cdfadb17fda5395b0742
bates <bates@694ef91d-65df-0310-b7bb-92e67a308ead>
2007-03-06 00:41:02 +0000

    Added splom.profile.nls method; new version
    git-svn-id: https://svn.r-project.org/R-packages/trunk@4173 694ef91d-65df-0310-b7bb-92e67a308ead

--- NRAIA/R/profile.R
+++ NRAIA/R/profile.R
@@ -15,6 +15,9 @@ as.data.frame.profile.nls <-
     fr
 }
 
+## extract only the y component from a prediction
+predy <- function(sp, vv) predict(sp, vv)$y
+
 ## A lattice-based plot method for profile.nls objects
 ## FIXME use pmax.int and pmin.int after 2.5.0 is released
 plot.profile.nls <-
@@ -30,8 +33,7 @@ plot.profile.nls <-
     bspl <- lapply(spl, splines::backSpline)
     tau <- c(-rev(levels), 0, levels)
     fr <- data.frame(tau = rep.int(tau, length(x)),
-                     pval = unlist(lapply(bspl,
-                     function(sp) predict(sp, x= tau)$y)),
+                     pval = unlist(lapply(bspl, predy, tau)),
                      pnm = gl(length(x), length(tau), labels = names(x)))
     ylab <- expression(tau)
     if (absVal) {
@@ -42,103 +44,17 @@ plot.profile.nls <-
            scales = list(x = list(relation = 'free')),
            ylab = ylab, xlab = "", panel = function(x, y, ...)
        {
-           pfun <- function(x) predict(spl[[panel.number()]], x = x)$y
+           pfun <- function(x) predy(spl[[panel.number()]], x)
            panel.grid(h = -1, v = -1)
            lsegments(x, y, x, 0, ...)
            if (absVal) {
                lsegments(x, y, rev(x), y)
-               pfun <- function(x) abs(predict(spl[[panel.number()]], x = x)$y)
+               pfun <- function(x) abs(predy(spl[[panel.number()]], x))
            } else {
                panel.abline(h = 0, ...)
            }
            panel.curve(pfun, ...)
        }, ...)
-}
-
-dp <- function(x = NULL,
-               varname = NULL, limits, at = NULL, lab = NULL,
-               draw = TRUE,
-
-               varname.col = add.text$col,
-               varname.cex = add.text$cex,
-               varname.lineheight = add.text$lineheight,
-               varname.font = add.text$font,
-               varname.fontfamily = add.text$fontfamily,
-               varname.fontface = add.text$fontface,
-               
-               axis.text.col = axis.text$col,
-               axis.text.alpha = axis.text$alpha,
-               axis.text.cex = axis.text$cex,
-               axis.text.font = axis.text$font,
-               axis.text.fontfamily = axis.text$fontfamily,
-               axis.text.fontface = axis.text$fontface,
-               
-               axis.line.col = axis.line$col,
-               axis.line.alpha = axis.line$alpha,
-               axis.line.lty = axis.line$lty,
-               axis.line.lwd = axis.line$lwd,
-               ...)
-{
-    j <- eval.parent(expression(j))
-    n.var <- eval.parent(expression(n.var))
-    add.text <- trellis.par.get("add.text")
-    axis.line <- trellis.par.get("axis.line")
-    axis.text <- trellis.par.get("axis.text")
-
-    if (!is.null(varname))
-        grid.text(varname,
-                  gp =
-                  gpar(col = varname.col,
-                       cex = varname.cex,
-                       lineheight = varname.lineheight,
-                       fontface = lattice:::chooseFace(varname.fontface,
-                       varname.font),
-                       fontfamily = varname.fontfamily))
-
-    if (draw)    
-    {
-        rot <- c(90, 0)
-        if (is.null(at))
-        {
-            at <- 
-                if (is.character(limits)) seq_along(limits)
-                else pretty(limits)
-        }
-        if (is.null(lab))
-        {
-            lab <- 
-                if (is.character(limits)) limits
-                else {
-                    rot <- 0
-                    format(at, trim = TRUE)
-                }
-        }
-        sides <- c("right", "bottom")
-        if (j == 1) sides <- "right"
-        if (j == n.var) sides <- "bottom"
-        for (side in sides)
-            panel.axis(side = side,
-                       at = at,
-                       labels = lab,
-                       tick = TRUE,
-                       check.overlap = TRUE,
-                       half = !(j %in% c(1, n.var)),
-
-                       tck = 1, ## from scales ?
-                       rot = rot, 
-
-                       text.col = axis.text.col,
-                       text.alpha = axis.text.alpha,
-                       text.cex = axis.text.cex,
-                       text.font = axis.text.font,
-                       text.fontfamily = axis.text.fontfamily,
-                       text.fontface = axis.text.fontface,
-
-                       line.col = axis.line.col,
-                       line.alpha = axis.line.alpha,
-                       line.lty = axis.line.lty,
-                       line.lwd = axis.line.lwd)
-    }
 }
 
 ## convert the x-cosine and y-cosine to an average and difference,
@@ -155,10 +71,7 @@ ad <- function(xc, yc)
 tauij <- function(xy, lev) lev * cos(xy$x + outer(xy$y/2, c(-1, 1)))
 
 ## safe arc-cosine
-sacos <- function(x) acos(pmax(-1, pmin(1, x)))
-
-## extract only the y component from a prediction
-predy <- function(sp, vv) predict(sp, vv)$y
+sacos <- function(x) acos(pmax(-0.999, pmin(0.999, x)))
 
 cont <- function(sij, sji, levels, nseg = 101)
 {
@@ -176,61 +89,159 @@ cont <- function(sij, sji, levels, nseg = 101)
 }
 
 splom.profile.nls <-
-    function (x, data,  ## unused - for compatibility with generic only
+    function (x, data, ## unused - only for compatibility with generic
               levels = sqrt(df[1] * qf(pmax(0, pmin(1, conf)), df[1], df[2])),
               conf = c(50, 80, 90, 95, 99)/100, ...)
 {
     df <- attr(x, "summary")$df
     levels <- sort(levels[is.finite(levels) && levels > 0])
     mlev <- max(levels)
-    pfr <- do.call("expand.grid", lapply(x, function(el) c(-mlev, mlev)))
     spl <- lapply(x, function(x)
                   interpSpline(x$par.vals[, attr(x, "parameters")$par], x$tau))
     bsp <- lapply(spl, backSpline)
+    pfr <- do.call("cbind", lapply(bsp, predy, c(-mlev, mlev)))
     fr <- as.data.frame(x)
     nms <- names(spl)
     for (nm in nms) fr[[nm]] <- predy(spl[[nm]], fr[[nm]])
+    np <- length(nms)
+    ## create a list of lists with the names of the parameters
+    traces <- lapply(x, function(el) lapply(x, function(el1) list()))
+    for (j in seq_along(nms)[-1]) {
+        for (i in seq_len(j - 1)) {
+            fri <- subset(fr, .pnm == nms[i])
+            sij <- interpSpline(fri[ , i], fri[ , j])
+            frj <- subset(fr, .pnm == nms[j])
+            sji <- interpSpline(frj[ , j], frj[ , i])
+            ll <- cont(sij, sji, levels)
+            traces[[j]][[i]] <- list(sij = sij, sji = sji, ll = ll)
+        }
+    }
+    ## panel function for lower triangle
     lp <- function(x, y, groups, subscripts, ...) {
-        i <- eval.parent(expression(i))
-        j <- eval.parent(expression(j))
-        fri <- subset(fr, .pnm == nms[i])
-        sij <- interpSpline(fri[ , i], fri[ , j])
-        frj <- subset(fr, .pnm == nms[j])
-        sji <- interpSpline(frj[ , j], frj[ , i])
-        psij <- predict(sij)
-        ll <- cont(sij, sji, levels)
+        tr <- traces[[eval.parent(expression(j))]][[eval.parent(expression(i))]]
+        pushViewport(viewport(xscale = c(-1.07, 1.07) * mlev,
+                              yscale = c(-1.07, 1.07) * mlev))
         dd <- sapply(current.panel.limits(), diff)/50
+        psij <- predict(tr$sij)
+        ll <- tr$ll
         ## now do the actual plotting
         panel.grid(h = -1, v = -1)
         llines(psij$y, psij$x, ...)
-        llines(predict(sji), ...)
+        llines(predict(tr$sji), ...)
         with(ll$tki, lsegments(y - dd[1], x, y + dd[1], x, ...))
         with(ll$tkj, lsegments(x, y - dd[2], x, y + dd[2], ...))
         for (k in seq_along(levels)) llines(ll$pts[k, , ], ...)
+        popViewport(1)
     }
+    ## panel function for upper triangle
     up <- function(x, y, groups, subscripts, ...) {
-        i <- eval.parent(expression(j)) ## plots are transposed
+        ## panels are transposed so reverse i and j
+        i <- eval.parent(expression(j))
         j <- eval.parent(expression(i))
-        fri <- subset(fr, .pnm == nms[i])
-        sij <- interpSpline(fri[ , i], fri[ , j])
-        frj <- subset(fr, .pnm == nms[j])
-        sji <- interpSpline(frj[ , j], frj[ , i])
-        psij <- predict(sij)
-        psji <- predict(sji)
-        ll <- cont(sij, sji, levels)
+        tr <- traces[[j]][[i]]
+        ll <- tr$ll
         pts <- ll$pts
-        ## do the actual plotting
-        pushViewport(viewport(xscale = range(predy(bsp[[i]], x)),
-                              yscale = range(predy(bsp[[j]], y))))
         limits <- current.panel.limits()
+        psij <- predict(tr$sij)
+        psji <- predict(tr$sji)        
+        ## do the actual plotting
         panel.grid(h = -1, v = -1)
         llines(predy(bsp[[i]], psij$y), predy(bsp[[j]], psij$x), ...)
         llines(predy(bsp[[i]], psji$x), predy(bsp[[j]], psji$y), ...)
         for (k in seq_along(levels))
             llines(predy(bsp[[i]], pts[k, , 1]),
                    predy(bsp[[j]], pts[k, , 2]), ...)
-        popViewport(1)
     }        
+    dp <- function(x = NULL,            # diagonal panel
+                   varname = NULL, limits, at = NULL, lab = NULL,
+                   draw = TRUE,
+
+                   varname.col = add.text$col,
+                   varname.cex = add.text$cex,
+                   varname.lineheight = add.text$lineheight,
+                   varname.font = add.text$font,
+                   varname.fontfamily = add.text$fontfamily,
+                   varname.fontface = add.text$fontface,
+                   
+                   axis.text.col = axis.text$col,
+                   axis.text.alpha = axis.text$alpha,
+                   axis.text.cex = axis.text$cex,
+                   axis.text.font = axis.text$font,
+                   axis.text.fontfamily = axis.text$fontfamily,
+                   axis.text.fontface = axis.text$fontface,
+                   
+                   axis.line.col = axis.line$col,
+                   axis.line.alpha = axis.line$alpha,
+                   axis.line.lty = axis.line$lty,
+                   axis.line.lwd = axis.line$lwd,
+                   ...)
+    {
+        j <- eval.parent(expression(j))
+        n.var <- eval.parent(expression(n.var))
+        add.text <- trellis.par.get("add.text")
+        axis.line <- trellis.par.get("axis.line")
+        axis.text <- trellis.par.get("axis.text")
+        
+        if (!is.null(varname))
+            grid.text(varname,
+                      gp =
+                      gpar(col = varname.col,
+                           cex = varname.cex,
+                           lineheight = varname.lineheight,
+                           fontface = lattice:::chooseFace(varname.fontface,
+                           varname.font),
+                           fontfamily = varname.fontfamily))
+        
+        if (draw)    
+        {
+            at <- pretty(limits)
+            sides <- c("left", "top")
+            if (j == 1) sides <- "top"
+            if (j == n.var) sides <- "left"
+            for (side in sides)
+                panel.axis(side = side,
+                           at = format(at, trim = TRUE),
+                           labels = format(at, trim = TRUE),
+                           tick = TRUE,
+                           check.overlap = TRUE,
+                           half = side == "top" && j > 1,
+                           
+                           tck = 1, rot = 0, 
+                           
+                           text.col = axis.text.col,
+                           text.alpha = axis.text.alpha,
+                           text.cex = axis.text.cex,
+                           text.font = axis.text.font,
+                           text.fontfamily = axis.text.fontfamily,
+                           text.fontface = axis.text.fontface,
+                           
+                           line.col = axis.line.col,
+                           line.alpha = axis.line.alpha,
+                           line.lty = axis.line.lty,
+                           line.lwd = axis.line.lwd)
+            lims <- c(-1.07, 1.07) * mlev
+            pushViewport(viewport(xscale = lims, yscale = lims))
+            side <- ifelse(j == 1, "right", "bottom")
+            which.half <- ifelse(j == 1, "lower", "upper")
+            at <- pretty(lims)
+            panel.axis(side = side, at = at, labels = format(at, trim = TRUE),
+                       tick = TRUE, half = TRUE, which.half = which.half,
+                       tck = 1, rot = 0,
+
+                       text.col = axis.text.col,
+                       text.alpha = axis.text.alpha,
+                       text.cex = axis.text.cex,
+                       text.font = axis.text.font,
+                       text.fontfamily = axis.text.fontfamily,
+                       text.fontface = axis.text.fontface,
+                           
+                       line.col = axis.line.col,
+                       line.alpha = axis.line.alpha,
+                       line.lty = axis.line.lty,
+                       line.lwd = axis.line.lwd)
+            popViewport(1)
+        }
+    }
 
     splom(~ pfr, lower.panel = lp, upper.panel = up, diag.panel = dp, ...)
 }
