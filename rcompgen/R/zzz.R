3f917db75b6a390057d10ce80e80b96f54b9cfcf
deepayan <deepayan@694ef91d-65df-0310-b7bb-92e67a308ead>
2007-04-08 23:28:10 +0000

    filename completion turned on by default in Windows
    git-svn-id: https://svn.r-project.org/R-packages/trunk@4282 694ef91d-65df-0310-b7bb-92e67a308ead

--- rcompgen/R/zzz.R
+++ rcompgen/R/zzz.R
@@ -11,7 +11,8 @@ assign("settings",
        list(ops = TRUE, ns = TRUE,
             args = TRUE, func = FALSE,
             ipck = FALSE, S3 = TRUE, data = TRUE,
-            help = TRUE, argdb = TRUE, files = FALSE),
+            help = TRUE, argdb = TRUE,
+            files = .Platform$OS.type == "windows"),
        env = .CompletionEnv)
 
 
