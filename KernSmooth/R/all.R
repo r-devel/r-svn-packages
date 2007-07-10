592e9c2019af0beda0a48aae4581e78d9c1cbffb
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2007-07-10 21:43:56 +0000

    tweaks
    git-svn-id: https://svn.r-project.org/R-packages/trunk@4598 694ef91d-65df-0310-b7bb-92e67a308ead

--- KernSmooth/R/all.R
+++ KernSmooth/R/all.R
@@ -329,9 +329,9 @@ cpblock <- function(X,Y,Nmax,q)
    out <- .Fortran(F_cp,as.double(X),as.double(Y),as.integer(n),
                    as.integer(qq),as.integer(Nmax),as.double(RSS),as.double(Xj),
                    as.double(Yj),as.double(coef),as.double(Xmat),as.double(wk),
-                   as.double(qraux),as.double(Cpvals))
+                   as.double(qraux),Cpvals=as.double(Cpvals))
 
-   Cpvec <- out[[14]]
+   Cpvec <- out$Cpvals
    Nval <- order(Cpvec)[1]
 
    return(Nval)
