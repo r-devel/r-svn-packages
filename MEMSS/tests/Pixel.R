dc67530d54ca77add7ec3d795b71a61b6ec9745b
bates <bates@694ef91d-65df-0310-b7bb-92e67a308ead>
2007-07-23 14:27:44 +0000

    Remove attribute and ordering, change factor levels to alphabetical, update test
    git-svn-id: https://svn.r-project.org/R-packages/trunk@4659 694ef91d-65df-0310-b7bb-92e67a308ead

--- MEMSS/tests/Pixel.R
+++ MEMSS/tests/Pixel.R
@@ -1,7 +1,7 @@
 library(MEMSS)
 options(show.signif.stars = FALSE)
-system.time(m1 <- lmer(pixel ~ day + I(day^2) + (1|Dog:Side) + (day|Dog),
-                       Pixel, verb = TRUE))
+m1 <- lmer(pixel ~ day + I(day^2) + (1|Dog:Side) + (day|Dog),
+           Pixel, verbose = TRUE)
 print(m1, corr = FALSE)
 ranef(m1)
 q("no")
