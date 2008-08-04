24df8db8ab110b67b1cf4d24f939c4a688421e48
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2008-08-04 08:32:10 +0000

    tweek SPSS missing values
    git-svn-id: https://svn.r-project.org/R-packages/trunk@5188 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/R/spss.R
+++ foreign/R/spss.R
@@ -60,8 +60,11 @@ read.spss <- function(file, use.value.labels = TRUE, to.data.frame = FALSE,
     if(!is.null(miss)) {
         if(reencode) {
             nm <- names(miss)
-            miss <- iconv(miss, cp, "")
+            nm <- iconv(nm, cp, "")
             names(miss) <- iconv(nm, cp, "")
+            for(i in seq_along(miss))
+                if(is.character(miss[[i]]$value))
+                   miss[[i]]$value <- iconv(miss[[i]]$value, cp, "")
             attr(rval, "missings") <- miss
         }
         if(use.missings)
@@ -71,28 +74,32 @@ read.spss <- function(file, use.value.labels = TRUE, to.data.frame = FALSE,
                 if(tp %in% c("one", "two", "three")) {
                     xi <- rval[[v]]
                     other <- miss[[v]]$value
+                    ## FIXME: do we need to worry about padding for string vals?
                     xi[xi %in% other] <- NA
                     rval[[v]] <- xi
+                    ## NB: untested from here down
                 } else if(tp == "low" || tp == "low+1") {
                     xi <- rval[[v]]
                     z <- miss[[v]]$value
-                    xi[xi <= z[1]] <- NA
-                    if(tp == "low+1") xi[xi == z[2]] <- NA
+                    if(tp == "low+1") xi[ xi <= z[1] | xi == z[2] ] <- NA
+                    else  xi[xi <= z[1]] <- NA
                     rval[[v]] <- xi
                 } else if(tp == "high" || tp == "high+1") {
                     xi <- rval[[v]]
                     z <- miss[[v]]$value
-                    xi[xi >= z[1]] <- NA
-                    if(tp == "high+1") xi[xi == z[2]] <- NA
+                    if(tp == "high+1") xi[ xi >= z[1] | xi == z[2] ] <- NA
+                    else xi[ xi >= z[1] ] <- NA
                     rval[[v]] <- xi
                 } else if(tp == "range" || tp == "range+1") {
                     xi <- rval[[v]]
                     z <- miss[[v]]$value
-                    xi[xi >= z[1] | xi <= z[2]] <- NA
-                    if(tp == "range+1") xi[xi == z[3]] <- NA
+                    if(tp == "range+1")
+                        xi[ xi >= z[1] | xi <= z[2] | xi[xi == z[3]] ] <- NA
+                    else
+                        xi[ xi >= z[1] | xi <= z[2] ] <- NA
                     rval[[v]] <- xi
                 } else
-                    warning(gettextf("missingness type %s is not yet handled", tp),
+                    warning(gettextf("missingness type %s is not handled", tp),
                             domain = NA)
         }
     } else use.missings <- FALSE
@@ -109,6 +116,8 @@ read.spss <- function(file, use.value.labels = TRUE, to.data.frame = FALSE,
             vl[[v]] <- iconv(vl[[v]], cp, "")
             names(vl[[v]]) <- iconv(nm2, cp, "")
         }
+        if(use.missings && !is.null(mv <- miss[[v]]$value))
+            vl[[v]] <- vl[[v]][! vl[[v]] %in% mv]
 	if (use.value.labels &&
 	    (!is.finite(max.value.labels) || nvalues <= max.value.labels) &&
 	    nlabels >= nvalues) {
