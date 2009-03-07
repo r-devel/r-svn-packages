94318e3be711018c81c19321c59bb072eea8a69a
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2009-03-07 11:55:38 +0000

    clean up of avl routines
    git-svn-id: https://svn.r-project.org/R-packages/trunk@5362 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/src/sfm-read.c
+++ foreign/src/sfm-read.c
@@ -226,7 +226,7 @@ free_value_label (struct value_label * v)
 }
 
 /* Frees value label P.  PARAM is ignored.  Used as a callback with
-   avl_destroy(). */
+   R_avl_destroy(). */
 void
 free_val_lab (void *p, void *param)
 {
@@ -244,7 +244,7 @@ free_dictionary (struct dictionary * d)
   d->splits = NULL;
 
   if (d->var_by_name)
-    avl_destroy (d->var_by_name, NULL);
+    R_avl_destroy (d->var_by_name, NULL);
 
   for (i = 0; i < d->nvar; i++)
     {
@@ -252,7 +252,7 @@ free_dictionary (struct dictionary * d)
 
       if (v->val_lab)
 	{
-	  avl_destroy (v->val_lab, free_val_lab);
+	  R_avl_destroy (v->val_lab, free_val_lab);
 	  v->val_lab = NULL;
 	}
       if (v->label)
@@ -1059,9 +1059,9 @@ read_variables (struct file_handle * h, struct variable *** var_by_index)
 
   /* Construct AVL tree of dictionary in order to speed up later
      processing and to check for duplicate varnames. */
-  dict->var_by_name = avl_create (cmp_variable, NULL);
+  dict->var_by_name = R_avl_create (cmp_variable, NULL);
   for (i = 0; i < dict->nvar; i++)
-    if (NULL != avl_insert (dict->var_by_name, dict->var[i]))
+    if (NULL != R_avl_insert (dict->var_by_name, dict->var[i]))
       lose ((_("%s: Duplicate variable name `%s' within system file"),
 	     h->fn, dict->var[i]->name));
 
@@ -1075,7 +1075,7 @@ lossage:
     }
   Free (dict->var);
   if (dict->var_by_name)
-    avl_destroy (dict->var_by_name, NULL);
+    R_avl_destroy (dict->var_by_name, NULL);
   Free (dict);
   ext->dict = NULL;
 
@@ -1257,12 +1257,12 @@ read_value_labels (struct file_handle * h, struct variable ** var_by_index)
 
       /* Create AVL tree if necessary. */
       if (!v->val_lab)
-	v->val_lab = avl_create (val_lab_cmp, (void *) (void *) (void *) (void *) (void *) (void *) (void *) (void *) (void *) &width);
+	v->val_lab = R_avl_create (val_lab_cmp, (void *) (void *) (void *) (void *) (void *) (void *) (void *) (void *) (void *) &width);
 
       /* Add each label to the variable. */
       for (j = 0; j < n_labels; j++)
 	{
-	  struct value_label *old = avl_replace (v->val_lab, cooked_label[j]);
+	  struct value_label *old = R_avl_replace (v->val_lab, cooked_label[j]);
 	  if (old == NULL)
 	    continue;
 
