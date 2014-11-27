906c5a01b725d06ad2986863e0bf156e32560f22
simon <simon@694ef91d-65df-0310-b7bb-92e67a308ead>
2014-11-27 13:35:35 +0000

    delete old tps eta code
    git-svn-id: https://svn.r-project.org/R-packages/trunk@6844 694ef91d-65df-0310-b7bb-92e67a308ead

--- mgcv/src/tprs.c
+++ mgcv/src/tprs.c
@@ -30,45 +30,6 @@ USA.*/
 
 void ErrorMessage(char *msg, int fatal);
 
-static inline double eta(int m,int d,double r)
-
-/* The basis functions for a thin plate spline for d- dimensional data, with an mth order 
-   wiggliness penalty. 
-   Note that r is really r^2 (saves a sqrt when d even, and some flops )
-*/
-
-{ double pi=PI,Ghalf=1.772453850905515881919; /* Gamma function of 0.5 = sqrt(pi) */
-  double f;
-  int i,k,d2,m2;
-
-  d2 = d/2;m2 = 2*m;
-  if (m2 <= d) ErrorMessage(_("You must have 2m>d for a thin plate spline."),1);
-  if (r<=0.0) return(0.0); /* this is safe: even if eta() gets inlined so that r comes in in an fp register! */
-  if (d%2==0) /* then d even */
-  { if ((m+1+d2)%2) f= -1.0; else f=1.0; /* finding (-1)^{m+1+d/2} */
-    for (i=0;i<m2-1;i++) f/=2;  /* dividing by 2^{2m-1} */
-    for (i=0;i<d2;i++) f/=pi;  /* dividing by pi^{d/2} */
-    for (i=2;i<m;i++) f/=i; /* dividing by (m-1)! */
-    for (i=2;i<=m-d2;i++) f/=i; /* dividing by (m-d/2)! */
-    /*f*=log(r);*/
-    f *= log(r) * .5; /* since r is really r^2 */
-    /* for (i=0;i<2*m-d;i++) f*=r;*/
-    for (i=0;i<m-d2;i++) f *= r; /* r^2m-d (noting r is really r^2) */
-  } else /* d odd */
-  { f=Ghalf;
-    k=m-(d-1)/2; /* 1/2 - d = d/2 -m */
-    for (i=0;i<k;i++) f/= -0.5-i; /* f = gamma function of d/2-m */
-    for (i=0;i<m;i++) f/= 4; /* divide by 2^{2m} */
-    for (i=0;i<d-1;i++) f/=pi;
-    f /= Ghalf;                /* dividing by (pi^{d/2}) */
-    for (i=2;i<m;i++) f/=i;  /* divide by (m-1)! */
-    /* for (i=0;i<2*m-d;i++) f*=r;*/
-    for (i=0;i<m-d2-1 ;i++) f *= r; /* note r really r^2 */
-    f *= sqrt(r); 
-  } 
-  return(f);
-}
-
 
 double eta_const(int m,int d) {
   /* compute the irrelevant constant for TPS basis */
