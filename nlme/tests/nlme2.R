d5d85b8ca1fdbd6f44f9a9e09365a45c9e91d51c
maechler <maechler@694ef91d-65df-0310-b7bb-92e67a308ead>
2015-12-02 12:13:52 +0000

    contrMat(): check result; others: more self-contained
    git-svn-id: https://svn.r-project.org/R-packages/trunk@7045 694ef91d-65df-0310-b7bb-92e67a308ead

--- nlme/tests/nlme2.R
+++ nlme/tests/nlme2.R
@@ -1,5 +1,6 @@
 library(nlme)
 
+options(digits = 7)
 ## https://stat.ethz.ch/pipermail/r-help/2014-September/422123.html
 nfm <- nlme(circumference ~ SSlogis(age, Asym, xmid, scal),
             data = Orange,
