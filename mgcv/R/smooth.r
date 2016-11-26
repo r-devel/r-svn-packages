2f9864ba8402cd0b9c799024f2c6ae6d6a7691d7
simon <simon@694ef91d-65df-0310-b7bb-92e67a308ead>
2016-11-26 20:56:25 +0000

    uniquecombs again
    git-svn-id: https://svn.r-project.org/R-packages/trunk@7295 694ef91d-65df-0310-b7bb-92e67a308ead

--- mgcv/R/smooth.r
+++ mgcv/R/smooth.r
@@ -157,6 +157,87 @@ uniquecombs <- function(x,ordered=FALSE) {
 ## `unique' now does this in R
   if (is.null(x)) stop("x is null")
   if (is.null(nrow(x))||is.null(ncol(x))) x <- data.frame(x)
+  recheck <- FALSE
+  if (inherits(x,"data.frame")) {
+    xoo <- xo <- x
+    ## reset character, logical and factor to numeric, to guarantee that text versions of labels
+    ## are unique iff rows are unique (otherwise labels containing "*" could in principle
+    ## fool it).
+    is.char <- rep(FALSE,length(x)) 
+    for (i in 1:length(x)) {
+      if (is.character(xo[[i]])) {
+        is.char[i] <- TRUE
+        xo[[i]] <- as.factor(xo[[i]])
+      }
+      if (is.factor(xo[[i]])||is.logical(xo[[i]])) x[[i]] <- as.numeric(xo[[i]])
+      if (!is.numeric(x[[i]])) recheck <- TRUE ## input contains unknown type cols 
+    }
+    #x <- data.matrix(xo) ## ensure all data are numeric
+  } else xo <- NULL
+  if (ncol(x)==1) { ## faster to use R 
+     xu <- if (ordered) sort(unique(x[,1])) else unique(x[,1])
+     ind <- match(x[,1],xu)
+     if (is.null(xo)) x <- matrix(xu,ncol=1,nrow=length(xu)) else {
+        x <-  data.frame(xu)
+	names(x) <- names(xo)
+     }
+  } else { ## no R equivalent that directly yields indices
+    if (ordered) {
+      chloc <- Sys.getlocale("LC_CTYPE")
+      Sys.setlocale("LC_CTYPE","C")
+    }
+    ## txt <- paste("paste0(",paste("x[,",1:ncol(x),"]",sep="",collapse=","),")",sep="")
+    ## ... this can produce duplicate labels e.g. x[,1] = c(1,11), x[,2] = c(12,2)...
+    ## solution is to insert separator not present in representation of a number (any
+    ## factor codes are already converted to numeric by data.matrix call above.)
+    txt <- paste("paste0(",paste("x[,",1:ncol(x),"]",sep="",collapse=",\"*\","),")",sep="")
+    xt <- eval(parse(text=txt)) ## text representation of rows
+    dup <- duplicated(xt)       ## identify duplicates
+    xtu <- xt[!dup]             ## unique text rows
+    x <- x[!dup,]               ## unique rows in original format
+    #ordered <- FALSE
+    if (ordered) { ## return unique in same order regardless of entry order
+      ## ordering of character based labels is locale dependent
+      ## so that e.g. running the same code interactively and via
+      ## R CMD check can give different answers. 
+      coloc <- Sys.getlocale("LC_COLLATE")
+      Sys.setlocale("LC_COLLATE","C")
+      ii <- order(xtu)
+      Sys.setlocale("LC_COLLATE",coloc)
+      Sys.setlocale("LC_CTYPE",chloc)
+      xtu <- xtu[ii]
+      x <- x[ii,]
+    }
+    ind <- match(xt,xtu)   ## index each row to the unique duplicate deleted set
+
+  }
+  if (!is.null(xo)) { ## original was a data.frame
+    x <- as.data.frame(x)
+    names(x) <- names(xo)
+    for (i in 1:ncol(xo)) {
+      if (is.factor(xo[,i])) { ## may need to reset factors to factors
+        xoi <- levels(xo[,i])
+        x[,i] <- if (is.ordered(xo[,i])) ordered(x[,i],levels=1:length(xoi),labels=xoi) else 
+                 factor(x[,i],levels=1:length(xoi),labels=xoi)
+        contrasts(x[,i]) <- contrasts(xo[,i])
+      }
+      if (is.char[i]) x[,i] <- as.character(x[,i])
+      if (is.logical(xo[,i])) x[,i] <- as.logical(x[,i])
+    }
+  }
+  if (recheck) {
+    if (all.equal(xoo,x[ind,],check.attributes=FALSE)!=TRUE) warning("uniquecombs has not worked properly")
+  }
+  attr(x,"index") <- ind
+  x
+} ## uniquecombs
+
+
+uniquecombs0 <- function(x,ordered=FALSE) {
+## takes matrix x and counts up unique rows
+## `unique' now does this in R
+  if (is.null(x)) stop("x is null")
+  if (is.null(nrow(x))||is.null(ncol(x))) x <- data.frame(x)
   if (inherits(x,"data.frame")) {
     xo <- x
     x <- data.matrix(xo) ## ensure all data are numeric
@@ -207,7 +288,7 @@ uniquecombs <- function(x,ordered=FALSE) {
   }
   attr(x,"index") <- ind
   x
-} ## uniquecombs
+} ## uniquecombs0
 
 cSplineDes <- function (x, knots, ord = 4,derivs=0)
 { ## cyclic version of spline design...
