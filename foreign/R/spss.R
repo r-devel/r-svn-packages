2f1d5936d922f054eaa21d5b2d85b771776eb283
ligges <ligges@694ef91d-65df-0310-b7bb-92e67a308ead>
2017-06-20 14:40:32 +0000

    new logic for handling value labels = factor levels from SPSS: fix for R >= 3.4.0 not allowing duplicated factor labels with two new args, new argument add.undeclared.levels for handling variables with a mixture of declared and undeclared value labels
    git-svn-id: https://svn.r-project.org/R-packages/trunk@7366 694ef91d-65df-0310-b7bb-92e67a308ead

--- foreign/R/spss.R
+++ foreign/R/spss.R
@@ -26,9 +26,14 @@
 read.spss <- function(file, use.value.labels = TRUE, to.data.frame = FALSE,
                       max.value.labels = Inf, trim.factor.names = FALSE,
                       trim_values = TRUE, reencode = NA,
-                      use.missings = to.data.frame, sub = ".", ...)
+                      use.missings = to.data.frame, sub = ".",
+                      add.undeclared.levels = c("sort", "append", "no"),
+                      duplicated.value.labels = c("append", "condense"),
+                      duplicated.value.labels.infix = "_duplicated_", ...)
 {
 
+    add.undeclared.levels <- match.arg(add.undeclared.levels)
+    duplicated.value.labels <- match.arg(duplicated.value.labels)
     trim <- function(strings, trim=TRUE)
         if (trim && is.character(strings)) sub(" +$", "", strings) else strings
 
@@ -165,19 +170,63 @@ read.spss <- function(file, use.value.labels = TRUE, to.data.frame = FALSE,
     has.vl <- which(!sapply(vl, is.null))
     for(v in has.vl) {
         nm <- names(vl)[[v]]
-        nvalues <- length(na.omit(unique(rval[[nm]])))
-        nlabels <- length(vl[[v]])
+        vlv <- vl[[v]]
+        nlabels <- length(vlv)
         if(reencode && nlabels) {
-            nm2 <- names(vl[[v]])
-            vl[[v]] <- iconv(vl[[v]], cp, "", sub=sub)
-            names(vl[[v]]) <- iconv(nm2, cp, "", sub=sub)
+            nm2 <- names(vlv)
+            vl[[v]] <- vlv <- iconv(vlv, cp, "", sub=sub)
+            names(vl[[v]]) <- names(vlv) <- iconv(nm2, cp, "", sub=sub)
         }
-        if (use.value.labels &&
-            (!is.finite(max.value.labels) || nvalues <= max.value.labels) &&
-            nlabels >= nvalues) {
-            rval[[nm]] <- factor(trim(rval[[nm]], trim_values),
-                                 levels = rev(trim(vl[[v]], trim_values)),
-                                 labels = rev(trim(names(vl[[v]]), trim.factor.names)))
+        
+        newlevels <- rev(trim(vlv, trim_values))
+        newrval <- trim(rval[[nm]], trim_values)
+
+        uniquevalues <- na.omit(unique(newrval))
+        nvalues <- length(uniquevalues)
+
+        ## We may have nlabels == nvalues but they do not always match,
+        ## e.g. in case 2 labels are duplicated, hence be careful:
+        if (use.value.labels && (add.undeclared.levels != "no" || all(uniquevalues %in% c(newlevels, ""))) &&
+            (!is.finite(max.value.labels) || nvalues <= max.value.labels)) {
+
+            newlabels <- rev(trim(names(vlv), trim.factor.names))
+            if(add.undeclared.levels != "no" && !all(uniquevalues %in% c(newlevels, ""))){
+                addlabels <- addlevels <- sort(uniquevalues[!(uniquevalues %in% c(newlevels, ""))])
+                newlevels <- c(newlevels, addlevels)
+                newlabels <- c(newlabels, addlabels)  
+                if(add.undeclared.levels == "sort"){              
+                    o <- order(newlevels)
+                    newlevels <- newlevels[o]
+                    newlabels <- newlabels[o]
+                }
+                warning("Undeclared level(s) ", paste(addlevels, collapse = ", "), " added in variable: ", nm)
+            }
+            dupnewlabels <- duplicated(newlabels)
+            ## duplicated factor labels are no longer possible for R >= 3.4.0,
+            ## hence adding two ways around
+            ## - append: appends infix plus original level (that is unique)
+            ## - condense: removes additional levels with identical labels and 
+            ##   condenses to the first of all duplicated levels
+            if(any(dupnewlabels)) {
+              warning("Duplicated levels in factor ", nm, ": ", 
+                      paste(newlabels[dupnewlabels], collapse=", "))
+              if(duplicated.value.labels == "append"){
+                newlabels[dupnewlabels] <- 
+                    paste(newlabels[dupnewlabels], newlevels[dupnewlabels], 
+                          sep = duplicated.value.labels.infix)
+              }
+              if(duplicated.value.labels == "condense"){
+                for(d in unique(newlabels[dupnewlabels])){
+                    dups <- newlabels %in% d
+                    newrval[newrval %in% newlevels[dups]] <- newlevels[dups][1]
+                }
+                newlabels <- newlabels[!dupnewlabels]
+                newlevels <- newlevels[!dupnewlabels]
+              }
+            }
+            rval[[nm]] <- factor(newrval,
+                                 levels = newlevels,
+                                 labels = newlabels)
         } else
             attr(rval[[nm]], "value.labels") <- vl[[v]]
     }
