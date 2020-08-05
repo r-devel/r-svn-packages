39c99f64188f64b69676585404e4791ab8592020
simon <simon@694ef91d-65df-0310-b7bb-92e67a308ead>
2020-08-05 21:55:13 +0000

    cox.ph hazard drop=FALSE
    git-svn-id: https://svn.r-project.org/R-packages/trunk@7871 694ef91d-65df-0310-b7bb-92e67a308ead

--- mgcv/R/coxph.r
+++ mgcv/R/coxph.r
@@ -114,7 +114,7 @@ cox.ph <- function (link = "identity") {
         if (p) {
           X <- (X - apply(gamma*X,2,cumsum)/cumsum(gamma))*wt ## Schoenfeld
 	  n <- nrow(X)
-	  R <- apply(X[n:1,],2,cumsum)[n:1,] ## score residuals
+	  R <- apply(X[n:1,,drop=FALSE],2,cumsum)[n:1,,drop=FALSE] ## score residuals
 	  ## now remove the penalization induced drift...
 	  x <- (1:n)[wt!=0]; dx <- seq(1,0,length=sum(wt!=0))
 	  drift <- approx(x,dx,xout=1:n,method="constant",rule=2)$y
@@ -144,9 +144,9 @@ cox.ph <- function (link = "identity") {
 	  a[,trind] <- matrix(oo$A[1:(p*nti)],p,nti)
 	  ## compute Schoenfeld resiudals, within stratum
           if (p) {
-            Xs <- X[ind,] <- (X[ind,] - apply(gamma[ind]*X[ind,],2,cumsum)/cumsum(gamma[ind]))*wt[ind]
+            Xs <- X[ind,,drop=FALSE] <- (X[ind,,drop=FALSE] - apply(gamma[ind]*X[ind,],2,cumsum)/cumsum(gamma[ind]))*wt[ind]
 	    n <- nrow(Xs)
-	    Rs <- apply(Xs[n:1,],2,cumsum)[n:1,] ## score residuals
+	    Rs <- apply(Xs[n:1,,drop=FALSE],2,cumsum)[n:1,,drop=FALSE] ## score residuals
 	    ## now remove the penalization induced drift...
 	    x <- (1:n)[wt[ind]!=0]; dx <- seq(1,0,length=sum(wt[ind]!=0))
 	    drift <- approx(x,dx,xout=1:n,method="constant",rule=2)$y
