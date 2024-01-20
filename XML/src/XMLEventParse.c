ce75a0944b39e5134bbce9979eb668a0610b3c1c
ripley <ripley@694ef91d-65df-0310-b7bb-92e67a308ead>
2024-01-20 10:31:26 +0000

    minimal changes for libxml2 2.12.x
    git-svn-id: https://svn.r-project.org/R-packages/trunk@8324 694ef91d-65df-0310-b7bb-92e67a308ead

--- XML/src/XMLEventParse.c
+++ XML/src/XMLEventParse.c
@@ -12,6 +12,12 @@
 #include <libxml/parserInternals.h>
 #endif
 
+#ifdef _WIN32
+/* on Unix, there is a configure check */
+# if LIBXML_VERSION < 20627 || LIBXML_VERSION > 21100
+#  define NO_CHECKED_ENTITY_FIELD 
+# endif
+#endif
 
 static USER_OBJECT_ createSAX2AttributesList(const xmlChar **attributes, int nb_attributes, int nb_defaulted, const xmlChar *encoding);
 
@@ -47,7 +53,7 @@ int RS_XML(isStandAloneHandler)(void *ctx);
 void RS_XML(warningHandler)(void *ctx, const char *msg, ...);
 void RS_XML(errorHandler)(void *ctx, const char *format, ...); 
 void RS_XML(fatalErrorHandler)(void *ctx, const char *msg, ...);
-void RS_XML(structuredErrorHandler)(void *ctx, xmlErrorPtr err);
+void RS_XML(structuredErrorHandler)(void *ctx, const struct _xmlError *err);
 
 
 
@@ -862,8 +868,9 @@ RS_XML(errorHandler)(void *ctx, const char *format, ...)
 
 }
 
+// was  RS_XML(structuredErrorHandler)(void *ctx, const xmlError err)
 void 
-RS_XML(structuredErrorHandler)(void *ctx, xmlErrorPtr err)
+RS_XML(structuredErrorHandler)(void *ctx, const struct _xmlError *err)
 {
    if(err->level == XML_ERR_FATAL) {
       Rf_error("Error in the XML event driven parser (line = %d, column = %d): %s",
